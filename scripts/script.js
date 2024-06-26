3452a553b61ec752fa22c657985b5b1f3ecee204
JNeo77 <lifted_2000@yahoo.com>
2024-06-26 08:09:58 -0400

    Style the calculator using CSS amd JavaScript
    Style the calculator using flexbox for the layout and choose a color
    theme for the buttons.
    Utilize 'mousedown' and 'mouseup' event listeners to toggle the button
    colors to simulate button 'pushing'.

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,8 @@ const squaredButton = document.querySelector(".squared");
 const plusMinus = document.querySelector(".plus-minus");
 const inverseButton = document.querySelector(".inverse");
 
+console.log(display.textContent);
+
 let displayArray = [];
 let currentOperator = '';
 let firstNumber = 0;
@@ -48,13 +50,17 @@ function operate (num1, operator, num2) {
       if (!num2) {
         return display.textContent = "Seriously?";
         break;
-      }
+      } else {
       return divide(num1, num2);
       break;
+      }
   }
 }
 
 function updateNumber () {
+  if (displayNumber.toString().length > 9) {
+    displayNumber = parseFloat(displayNumber).toExponential(2);
+  }
   display.textContent = displayNumber;
   currentOperator ? secondNumber = displayNumber : firstNumber = displayNumber;
 }
@@ -71,26 +77,55 @@ for (let i = 0; i < numberButtons.length; i++) {
   numberButtons[i].addEventListener("click", () => {
     const numberText = numberButtons[i].textContent;
     displayArray.push(numberText);
-    displayNumber = parseInt(displayArray.join(''));
+    displayNumber = parseFloat(displayArray.join(''));
     updateNumber();
+  });
+
+  numberButtons[i].addEventListener("mousedown", () => {
+    numberButtons[i].classList.toggle("number-pressed");
+  });
+
+  numberButtons[i].addEventListener("mouseup", () => {
+    numberButtons[i].classList.toggle("number-pressed");
   });
 }
 
 for (let i = 0; i < operatorButtons.length; i++) {
   operatorButtons[i].addEventListener("click", () => {
     if (currentOperator) {
+      if (!secondNumber) { secondNumber = firstNumber};
       displayNumber = operate(firstNumber, currentOperator, secondNumber);
+      if (displayNumber.toString().length > 9) {
+        displayNumber = parseFloat(displayNumber).toExponential(2);
+      }
       display.textContent = displayNumber;
       firstNumber = displayNumber;
+      currentOperator = "";
     }
     currentOperator = operatorButtons[i].textContent;
     displayArray = [];
+  });
+
+  operatorButtons[i].addEventListener("mousedown", () => {
+    operatorButtons[i].classList.toggle("operator-pressed");
+  });
+
+  operatorButtons[i].addEventListener("mouseup", () => {
+    operatorButtons[i].classList.toggle("operator-pressed");
   });
 }
 
 squaredButton.addEventListener("click", () => {
   displayNumber *= displayNumber;
   updateNumber();
+});
+
+squaredButton.addEventListener("mousedown", () => {
+  squaredButton.classList.toggle("clear-pressed");
+});
+
+squaredButton.addEventListener("mouseup", () => {
+  squaredButton.classList.toggle("clear-pressed");
 });
 
 inverseButton.addEventListener("click", () => {
@@ -103,22 +138,58 @@ inverseButton.addEventListener("click", () => {
   }
 });
 
+inverseButton.addEventListener("mousedown", () => {
+  inverseButton.classList.toggle("clear-pressed");
+});
+
+inverseButton.addEventListener("mouseup", () => {
+  inverseButton.classList.toggle("clear-pressed");
+});
+
 plusMinus.addEventListener("click", () => {
   displayNumber *= -1;
   updateNumber();
+});
+
+plusMinus.addEventListener("mousedown", () => {
+  plusMinus.classList.toggle("plus-minus-pressed");
+});
+
+plusMinus.addEventListener("mouseup", () => {
+  plusMinus.classList.toggle("plus-minus-pressed");
 });
 
 equalButton.addEventListener("click", () => {
   if (currentOperator) {
     if (!secondNumber) { secondNumber = firstNumber};
     displayNumber = operate(firstNumber, currentOperator, secondNumber);
+    if (displayNumber.toString().length > 9) {
+      displayNumber = parseFloat(displayNumber).toExponential(2);
+    }
     display.textContent = displayNumber;
     firstNumber = displayNumber;
   }
   currentOperator = "";
+  displayArray = [];
+});
+
+equalButton.addEventListener("mousedown", () => {
+  equalButton.classList.toggle("plus-minus-pressed");
+});
+
+equalButton.addEventListener("mouseup", () => {
+  equalButton.classList.toggle("plus-minus-pressed");
 });
 
 clearButton.addEventListener("click", () => {
   display.textContent = 0;
   clear();
+});
+
+clearButton.addEventListener("mousedown", () => {
+  clearButton.classList.toggle("clear-pressed");
+});
+
+clearButton.addEventListener("mouseup", () => {
+  clearButton.classList.toggle("clear-pressed");
 });
